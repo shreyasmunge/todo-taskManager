940853645761b3c0fc9215b51c251757b843714e
Shreyas Munge <shreyasmunge7@gmail.com>
2025-07-23 15:09:21 +0530

    create user model and login

---
+++ todo_project/tasks/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user)
+            return redirect('task_list')
+    return render(request, 'login.html')
+
+def logout(request):
+    logout(request)
+    return redirect('login')
+
